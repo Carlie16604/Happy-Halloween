d325fd88d29af2f6b92833ddf6aba383ba91bbdf
Carlie16604 <carliemaloneco@gmail.com>
2023-09-20 21:13:44 -0600

    ~Spooky Submission~

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const client = new pg.Client('postgres://localhost/halloween')
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const morgan = require("morgan")
 app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
 
 app.get('/api/halloween', async(req, res, next) => {
     try {
@@ -14,7 +17,7 @@ app.get('/api/halloween', async(req, res, next) => {
         const response = await client.query(SQL)
         res.send(response.rows)
     } catch (error) {
-        next(error)
+        next(error) 
     }
 });
 
@@ -26,7 +29,66 @@ app.get('/api/halloween/:id', async (req,res,next) => {
             WHERE id=$1
         `;
         const response = await client.query(SQL, [req.params.id])
-        res.send(response.rows[0])
+        if(response.rows.length === 0){
+            throw new Error("ID does not exist dude")
+        }
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+});
+
+app.post('/api/halloween', async (req, res, next) => {
+    const body = req.body
+    // you dont actually need this ^^^
+    try {
+        const SQL = `
+        INSERT INTO halloween(name, type, count)
+        VALUES($1, $2, $3)
+        RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.name, req.body.type, req.body.count])
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+});
+
+app.put('api/halloween', async (req,res, next) => {
+    try {
+        const SQL = `
+            UPDATE halloween
+            SET name = $1, type = $2, count = $3
+            WHERE id=$4
+            RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.name, req.body.type, req.body.count, req.params.id])
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+});
+
+//error handling: custom 404 route
+app.use('*', (req, res, next) => {
+    res.status(404).send("Invalid Route Silly Goober")
+});
+
+//error handling: custom
+app.use((err, req, res, next) => {
+    console.log("error handler")
+    res.status(500).send(err.message)
+});
+
+app.delete('api/halloween/:id', async (req,res,next) => {
+    try {
+        const SQL = `
+            DELETE 
+            FROM halloween
+            WHERE id=$1
+        `;
+        const response = await client.query(SQL, [req.params.id])
+        res.send(response.rows)
     } catch (error) {
         next(error)
     }
